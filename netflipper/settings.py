424df7bf5637987294c657b68147eadf2ea5ef47
colinmipapi <colinjmcfaul@gmail.com>
2017-07-10 01:57:35 -0400

    First commit

--- netflipper/settings.py
+++ netflipper/settings.py
@@ -96,6 +96,11 @@ REST_FRAMEWORK = {
     ]
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
